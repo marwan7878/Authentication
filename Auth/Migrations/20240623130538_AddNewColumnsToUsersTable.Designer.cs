70ccdb2bc4c99b6c06fea87466bc543f6fb25c61
marwan7878 <marwanmohamed7887@gmail.com>
2024-06-23 17:43:24 +0300

    correct login

--- Auth/Migrations/20240623130538_AddNewColumnsToUsersTable.Designer.cs
+++ Auth/Migrations/20240623130538_AddNewColumnsToUsersTable.Designer.cs
@@ -4,6 +4,7 @@ using Auth.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,13 +12,15 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Auth.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240623130538_AddNewColumnsToUsersTable")]
+    partial class AddNewColumnsToUsersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.4")
+                .HasAnnotation("ProductVersion", "8.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
@@ -75,6 +78,7 @@ namespace Auth.Data.Migrations
                         .HasColumnType("bit");
 
                     b.Property<byte[]>("ProfilePicture")
+                        .IsRequired()
                         .HasColumnType("varbinary(max)");
 
                     b.Property<string>("SecurityStamp")
--- Auth/Models/ApplicationDbContext.cs
+++ Auth/Models/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Auth.Models
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -13,14 +13,17 @@ namespace Auth.Models
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
 			base.OnModelCreating(builder);
-            builder.Entity<IdentityUser>().ToTable("Users","Security");
+            builder.Entity<ApplicationUser>().ToTable("Users","Security");
             builder.Entity<IdentityRole>().ToTable("Roles" , "Security");
             builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "Security");
             builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "Security");
             builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "Security");
             builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "Security");
             builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "Security");
+
+            
 		}
+
 
 
 	}
