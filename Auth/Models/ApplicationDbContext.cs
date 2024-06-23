d16731e8fb67f5976694b146fcac16bc0eacc76d
marwan7878 <marwanmohamed7887@gmail.com>
2024-06-23 15:52:27 +0300

    modify name of identity tables

--- Auth/Models/ApplicationDbContext.cs
+++ Auth/Models/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Auth.Models
@@ -9,5 +10,19 @@ namespace Auth.Models
             : base(options)
         {
         }
-    }
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+            builder.Entity<IdentityUser>().ToTable("Users","Security");
+            builder.Entity<IdentityRole>().ToTable("Roles" , "Security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "Security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "Security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "Security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "Security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "Security");
+		}
+
+
+	}
+    
 }
