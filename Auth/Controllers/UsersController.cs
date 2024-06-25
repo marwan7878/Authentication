5d974d2fff97985abf98172384d09238c41e2869
marwan7878 <marwanmohamed7887@gmail.com>
2024-06-26 02:08:20 +0300

    show all users with its roles

---
+++ Auth/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Auth.Models;
+using Auth.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager,RoleManager<IdentityRole> roleManager) 
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<UserViewModel> users = await _userManager.Users.Select(user=> new UserViewModel
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Username = user.UserName,
+                Email = user.Email,
+                Roles = _userManager.GetRolesAsync(user).Result
+            }).ToListAsync();
+
+            return View(users);
+        }
+    }
+}
