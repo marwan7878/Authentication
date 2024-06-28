ef972f3580a9b24cd0a473c976a6b41b52ce699f
marwan7878 <marwanmohamed7887@gmail.com>
2024-06-28 21:34:48 +0300

    make custom attribute to validate roles check

--- Auth/Controllers/UsersController.cs
+++ Auth/Controllers/UsersController.cs
@@ -90,7 +90,7 @@ namespace Auth.Controllers
         [AutoValidateAntiforgeryToken]
         public async Task<IActionResult> Create(AddUserViewModel userVM)
         {
-            if (!ModelState.IsValid) return View(ModelState);
+            if (!ModelState.IsValid) return View(userVM);
             
             if (!userVM.Roles.Any(r => r.IsSelected))
             {
@@ -100,6 +100,11 @@ namespace Auth.Controllers
             if(await _userManager.FindByEmailAsync(userVM.Email) != null)
             {
                 ModelState.AddModelError("Email","This email is already exists!");
+                return View(userVM);
+            }
+            if(await _userManager.FindByNameAsync(userVM.Email) != null)
+            {
+                ModelState.AddModelError("Username","This username is already exists!");
                 return View(userVM);
             }
             var user = new ApplicationUser
@@ -124,12 +129,65 @@ namespace Auth.Controllers
 
             return RedirectToAction(nameof(Index));
         }
-
+        //remote attribute
         public async Task<IActionResult> CheckEmail(string email)
         {
             if (await _userManager.FindByEmailAsync(email) == null)
                 return Json(true);
             return Json(false);
+        }
+
+        public async Task<IActionResult> Edit(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return NotFound();
+
+            var roles = await _roleManager.Roles.ToListAsync();
+            var userVM = new EditUserViewModel
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Username = user.UserName,
+                Roles = roles.Select(r => new RoleViewModel
+                {
+                    RoleId = r.Id,
+                    RoleName = r.Name,
+                    IsSelected = _userManager.IsInRoleAsync(user, r.Name).Result
+                }).ToList()
+            };
+            return View(userVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            if (!model.Roles.Any(r => r.IsSelected))
+            {
+                ModelState.AddModelError("Roles", "Please select at least one role");
+                return View(model);
+            }
+
+
+            var user = await _userManager.FindByIdAsync(model.Id);
+            if (user == null) return NotFound();
+
+            await _userManager.UpdateAsync(user);
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            foreach (var role in model.Roles)
+            {
+                if (userRoles.Any(r => r == role.RoleName) && !role.IsSelected)
+                    await _userManager.RemoveFromRoleAsync(user, role.RoleName);
+                if (userRoles.Any(r => r != role.RoleName) && role.IsSelected)
+                    await _userManager.AddToRoleAsync(user, role.RoleName);
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
